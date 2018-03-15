1fe94f366c759744227d3ce1142569c96bfe7db4
abhi1227 <abhiagarwal1227@gmail.com>
2018-03-15 11:23:54 +0530

    first commit

---
+++ app/src/main/java/com/rbbn/ems/app/LoginActivity.java
@@ -0,0 +1,165 @@
+package com.rbbn.ems.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.rbbn.ems.R;
+import com.rbbn.ems.utils.PerformNetworkOperations;
+
+public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * Id to identity READ_CONTACTS permission request.
+     */
+    private static final int REQUEST_READ_CONTACTS = 0;
+
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     * TODO: remove after connecting to a real authentication system.
+     */
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello", "bar@example.com:world"
+    };
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    // UI references.
+    private EditText mEmailView;
+    private EditText mPasswordView;
+    private EditText mEMSIpview;
+
+    private String launchURL;
+    private String getAuthURL;
+    private String logonURL;
+    private String securityURL;
+    private String authLoginURL;
+    private String error;
+    private Button mEmailSignInButton;
+
+    private String token;
+    private PerformNetworkOperations performNetworkOperations;
+    private static LoginActivity instance;
+
+    public static LoginActivity getInstance() {
+        return instance;
+    }
+
+    public static void setInstance(LoginActivity instance) {
+        LoginActivity.instance = instance;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+        setInstance(this);
+        // Set up the login form.
+        mEmailView = (EditText) findViewById(R.id.username);
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mEMSIpview = (EditText) findViewById(R.id.ipAddress);
+        mEmailSignInButton = (Button) findViewById(R.id.loginBtn);
+
+        launchURL = getResources().getString(R.string.launch_url);
+        getAuthURL = getResources().getString(R.string.auth_session);
+        logonURL = getResources().getString(R.string.logon_url);
+        securityURL = getResources().getString(R.string.security_url);
+        authLoginURL = getResources().getString(R.string.auth_login);
+
+        performNetworkOperations = new PerformNetworkOperations(this);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+
+    }
+
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+        mEMSIpview.setError(null);
+        // Store values at the time of the login attempt.
+        final String emsIP = mEMSIpview.getText().toString();
+        final String email = mEmailView.getText().toString();
+        final String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if (TextUtils.isEmpty(emsIP)) {
+            mEMSIpview.setError("Please enter an ems ip");
+            focusView = mEMSIpview;
+            cancel = true;
+        }
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            performNetworkOperations.doLogin(emsIP, email, password);
+//            Toast.makeText(this,token,Toast.LENGTH_SHORT).show();
+//            List<String> nodeData = performNetworkOperations.getNodesList(emsIP,token);
+
+        }
+    }
+
+    public void navigateActivity(List<String> nodeDataList, String emsIp) {
+        View view = mEmailSignInButton;
+        ActivityOptionsCompat options = ActivityOptionsCompat.
+                makeSceneTransitionAnimation(this, view, "transition");
+        int revealX = (int) (view.getX() + view.getWidth() / 2);
+        int revealY = (int) (view.getY() + view.getHeight() / 2);
+
+        Intent intent = new Intent(this, DemoLikeTumblrActivity.class);
+        intent.putExtra(DemoLikeTumblrActivity.EXTRA_CIRCULAR_REVEAL_X, revealX);
+        intent.putExtra(DemoLikeTumblrActivity.EXTRA_CIRCULAR_REVEAL_Y, revealY);
+        intent.putStringArrayListExtra("NodeData", (ArrayList<String>) nodeDataList);
+        intent.putExtra("EMSIP", emsIp);
+        ActivityCompat.startActivity(this, intent, options.toBundle());
+    }
+
+}
---
+++ app/src/main/java/com/rbbn/ems/utils/NetworkUtils.java
@@ -0,0 +1,113 @@
+package com.rbbn.ems.utils;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+/**
+ * Created by aaagarwal on 09-03-2018.
+ */
+
+public class NetworkUtils {
+    private static NetworkUtils mInstance;
+    private RequestQueue mRequestQueue;
+    private static ProgressDialog pDialog;
+    private static Context mCtx;
+
+    private NetworkUtils(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+
+    }
+
+    public static synchronized NetworkUtils getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new NetworkUtils(context);
+            new NukeSSLCerts().nuke();
+            pDialog = new ProgressDialog(context);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+
+    public static class NukeSSLCerts {
+        protected static final String TAG = "NukeSSLCerts";
+
+        public static void nuke() {
+            try {
+                TrustManager[] trustAllCerts = new TrustManager[]{
+                        new X509TrustManager() {
+                            public X509Certificate[] getAcceptedIssuers() {
+                                X509Certificate[] myTrustedAnchors = new X509Certificate[0];
+                                return myTrustedAnchors;
+                            }
+
+                            @Override
+                            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                            }
+
+                            @Override
+                            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                            }
+                        }
+                };
+
+                SSLContext sc = SSLContext.getInstance("SSL");
+                sc.init(null, trustAllCerts, new SecureRandom());
+                HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+                HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                    @Override
+                    public boolean verify(String arg0, SSLSession arg1) {
+                        return true;
+                    }
+                });
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    public void showpDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    public void hidepDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
+}
